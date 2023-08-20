bfb7f42426807dfaf63b011f4fe824b6f8a60540
Pessi <pessiv@outlook.com>
2023-08-20 13:24:16 +0300

    ADDED_TODOS

--- game.c
+++ game.c
@@ -1,5 +1,17 @@
-#include<SDL2/SDL_video.h>
+#include<SDL2/SDL.h>
 
 int main(void)  {
-	SDL_CreateWindow("haista paska",50,50,50,50,SDL_WINDOW_RESIZABLE);
+	//TODO: draw candies
+	//TODO: register mouse clicks
+	//TODO: check if candies can be destroyed based on the last two clicks
+	//TODO: refresh screen
+	//TODO: draw a click marker
+	//TODO: destroy candies accordingly
+	//TODO: game loop
+	SDL_CreateWindow("haista paska",50,50,50,50,SDL_WINDOW_FULLSCREEN);
+	SDL_ShowSimpleMessageBox( SDL_MESSAGEBOX_INFORMATION,
+				"Hellow World",
+				"asdasjkl_fuckyou",
+				NULL);
+	SDL_Delay(2000);
 }
